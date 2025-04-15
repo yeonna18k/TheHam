bd2e72b0053b16e007c7c5531b549dea2cf1abae
yeonna18k <yeonna18k@gmail.com>
2025-04-15 15:51:40 +0900

    feat: 뷰포트 설정 추가 및 레이아웃 코드 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,27 @@
+import { Viewport } from "next";
 import "./globals.css";
 
 export const metadata = {
-  title: 'My PWA App',
-  description: 'Next.js App Router 기반 PWA',
-  manifest: '/manifest.json',
-  themeColor: '#000000',
+  title: "My PWA App",
+  description: "Next.js App Router 기반 PWA",
+  manifest: "/manifest.json",
   icons: {
-    icon: '/icons/icon-192x192.png',
-    apple: '/icons/icon-192x192.png',
+    icon: "/icons/icon-192x192.png",
+    apple: "/icons/icon-192x192.png",
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const viewport: Viewport = {
+  themeColor: "black",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   );
