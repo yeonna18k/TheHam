ca314112a214bc40d55fed5557402e49f9d3dbd7
yeonna18k <yeonna18k@gmail.com>
2025-04-16 19:25:24 +0900

    feat: 프로필 페이지에 탭 및 내용 추가

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -7,15 +7,14 @@ export default function ProfileLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section>
+    <section className="bg-gray-100 h-screen">
       <header className="relative py-3 border-b border-gray-200 title2 flex items-center justify-center">
         <Link className="absolute left-3" href="/main">
           <ChevronLeft size={24} />
         </Link>
         프로필
       </header>
-
-      {children}
+      <section className="h-full px-4 py-6">{children}</section>
     </section>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,56 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const PROFILE_TABS = [
+  {
+    value: "account",
+    label: "계정 정보",
+    title: "계정 정보",
+    description: "계정에 대한 정보",
+  },
+  {
+    value: "achievements",
+    label: "업적",
+    title: "나의 업적",
+    description: "지금까지 획득한 업적 목록이에요",
+  },
+  {
+    value: "statistics",
+    label: "통계",
+    title: "챌린지 통계",
+    description: "지금까지 참여한 챌린지 통계예요",
+  },
+  {
+    value: "warnings",
+    label: "경고",
+    title: "과소비 경고",
+    description: "예산 한도가 얼마나 남았는지 알려드려요",
+  },
+];
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList className="w-fit ">
+        {PROFILE_TABS.map((tab) => (
+          <TabsTrigger key={tab.value} value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {PROFILE_TABS.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          <section className="rounded-lg shadow-sm bg-white py-6 px-3 flex flex-col gap-3">
+            <div
+              className={`${
+                tab.value === "account" ? "hidden" : "block"
+              } flex flex-col gap-3`}
+            >
+              <h1 className="title1">{tab.title}</h1>
+              <p className="body1 text-gray-500">{tab.description}</p>
+            </div>
+          </section>
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
 }
