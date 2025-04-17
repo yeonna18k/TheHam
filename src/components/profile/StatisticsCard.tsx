4957d83fae110c1bb0c6e7926caad8ad8fab68f6
yeonna18k <yeonna18k@gmail.com>
2025-04-17 18:24:53 +0900

    feat: 통계 카드 컴포넌트 추가

--- src/components/profile/StatisticsCard.tsx
+++ src/components/profile/StatisticsCard.tsx
@@ -1,3 +1,26 @@
-export default function StatisticsCard() {
-  return <div>StatisticsCard</div>;
+import { cn } from "@/lib/utils";
+import { ChallengeStatistics } from "@/mock/challengeStatisticsData";
+import { PiggyBank } from "lucide-react";
+
+export default function StatisticsCard({
+  type,
+  description,
+  amount,
+  count,
+}: ChallengeStatistics) {
+  return (
+    <div className="p-4 border border-gray-200 rounded-md flex gap-2 flex-col items-center">
+      <div className="w-9 h-9 rounded-full bg-primary/20 flex items-center justify-center shrink-0">
+        <PiggyBank size={24} className="text-primary" />
+      </div>
+      <p className="body1 text-gray-500">{description}</p>
+      <h1
+        className={cn("h1", {
+          "text-primary": type === "amount",
+        })}
+      >
+        {type === "amount" ? `₩ ${amount?.toLocaleString()}` : count}
+      </h1>
+    </div>
+  );
 }
