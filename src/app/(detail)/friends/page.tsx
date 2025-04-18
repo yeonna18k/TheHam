baa5c679935075c7bf64eddb1687846e05b53308
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-04-18 14:19:06 +0900

    수정의수정의수정의수정의수정

--- src/app/(detail)/friends/page.tsx
+++ src/app/(detail)/friends/page.tsx
@@ -1,20 +1,11 @@
 import FriendsContainer from "@/components/friends/FriendsContainer";
-
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "친구 관리",
   description: "친구 관리 및 챌린지 초대",
 };
 
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-};
-
-const FriendsPage: React.FC = () => {
+export default function FriendsPage() {
   return <FriendsContainer />;
-};
-
-export default FriendsPage;
+}
