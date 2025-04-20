40571914b7a1656b074b365e967fa5319c20e994
yeonna18k <yeonna18k@gmail.com>
2025-04-20 11:02:08 +0900

    feat: 거래 내역 페이지 및 레이아웃 컴포넌트 추가

---
+++ src/app/(detail)/transactions/page.tsx
@@ -0,0 +1,31 @@
+import ExpenditureContainer from "@/components/transactions/ExpenditureContainer";
+import IncomeContainer from "@/components/transactions/IncomeContainer";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const REGISTER_TABS = [
+  { value: "expenditure", label: "지출" },
+  { value: "income", label: "수입" },
+];
+
+export default function Register() {
+  return (
+    <Tabs defaultValue="expenditure" className="w-full">
+      <TabsList className="w-fit ">
+        {REGISTER_TABS.map((tab) => (
+          <TabsTrigger key={tab.value} value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {REGISTER_TABS.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}>
+          {tab.value === "expenditure" ? (
+            <ExpenditureContainer />
+          ) : (
+            <IncomeContainer />
+          )}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
