11f4717696aca5398eda44b7db8dccbc58d6d86f
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2025-04-22 13:35:53 +0900

    fix: 오류 수정

--- src/api/FcmControllerAPI.ts
+++ src/api/FcmControllerAPI.ts
@@ -1,9 +1,11 @@
 "use Client" 
 
-import { baseFetch } from "./BaseAPI"
-import axios from "axios";
+import { baseFetch } from './BaseAPI';
 
+// FCM 토큰 전송
 export async function FcmController(token: string) {
-    const response = await axios.post(`${baseFetch}/api/v1/fcm/token`, token);
-    return response.data;
+  return baseFetch<void>('/api/v1/fcm/token', {
+    method: 'POST',
+    data: { token },
+  });
 }
--- src/api/FriendsAPI.ts
+++ src/api/FriendsAPI.ts
@@ -1,17 +1,20 @@
-"use Client" 
+"use client"
 
-import { baseFetch } from "./BaseAPI"
-import axios from "axios";
+import { baseFetch } from './BaseAPI';
 
-//카카오톡 공유하기 링크
+// 카카오톡 공유하기 링크
 export async function SharedKakao(token: string) {
-    const response = await axios.get(`${baseFetch}/api/v1/friends/invite`, { headers: { Authorization: `Bearer ${token}` } });
-    return response.data;
+  return baseFetch('/api/v1/friends/invite', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
 
-//친구 초대 토큰 발급
+// 친구 초대 토큰 발급
 export async function FriendsGetToken() {
-    const response = await axios.get(`${baseFetch}/api/v1/friends/invite/token`);
-    return response.data;
+  return baseFetch('/api/v1/friends/invite/token', {
+    method: 'GET',
+  });
 }
-
