e18770a874c2449875821d9abc03dbe8deb4dc08
yeonna18k <yeonna18k@gmail.com>
2025-04-24 18:54:07 +0900

    feat: authApi 및 profileApi 파일 추가, 회원가입 및 프로필 조회 기능 구현

---
+++ src/api/profileApi.ts
@@ -0,0 +1,28 @@
+import { UsersProfileRequest, UsersProfileResponse } from '@/types/profile';
+import { baseFetch } from './BaseAPI';
+
+export const getUsersProfile = async (): Promise<UsersProfileResponse> => {
+  try {
+    return await baseFetch<UsersProfileResponse>(`/users/profile`, {
+      method: 'GET',
+    });
+  } catch (error) {
+    console.error('유저 프로필 조회 에러:', error);
+    throw error;
+  }
+};
+
+export const putUsersProfile = async (requestData: UsersProfileRequest) => {
+  try {
+    return await baseFetch<UsersProfileResponse>(`/users/profile`, {
+      method: 'PUT',
+      data: {
+        nickname: requestData.nickname,
+        categories: requestData.profileImageUrl,
+      },
+    });
+  } catch (error) {
+    console.error('유저 프로필 수정 에러:', error);
+    throw error;
+  }
+};
