53f9ad91ac1a3e3c01ec04a94a0441725f6289e5
yeonna18k <yeonna18k@gmail.com>
2025-04-25 17:35:46 +0900

    feat: 거래 내역 및 탭 레이아웃 추가

---
+++ src/app/(detail)/transactions/tabs/layout.tsx
@@ -0,0 +1,10 @@
+import DetailHeader from '@/components/layout/DetailHeader';
+
+export default function layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <DetailHeader>내역</DetailHeader>
+      <section className="h-full px-4 py-6">{children}</section>
+    </>
+  );
+}
---
+++ src/app/(detail)/transactions/tabs/page.tsx
@@ -0,0 +1,24 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+const TRANSACTIONS_TABS = [
+  { value: 'date', label: '기간 설정' },
+  { value: 'calendar', label: '캘린더' },
+  { value: 'budget', label: '예산 설정' },
+];
+
+export default function Register() {
+  return (
+    <Tabs defaultValue="date" className="w-full">
+      <TabsList className="w-fit ">
+        {TRANSACTIONS_TABS.map((tab) => (
+          <TabsTrigger key={tab.value} value={tab.value}>
+            {tab.label}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {TRANSACTIONS_TABS.map((tab) => (
+        <TabsContent key={tab.value} value={tab.value}></TabsContent>
+      ))}
+    </Tabs>
+  );
+}
