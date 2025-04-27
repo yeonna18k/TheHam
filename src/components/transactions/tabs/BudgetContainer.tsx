43a693c891715d204ff9b67b7a3837d26f787607
yeonna18k <yeonna18k@gmail.com>
2025-04-28 00:48:45 +0900

    feat: 가계부 UI 개선 및 '@tanstack/react-virtual' 패키지 추가

--- src/components/transactions/tabs/BudgetContainer.tsx
+++ src/components/transactions/tabs/BudgetContainer.tsx
@@ -1,3 +1,12 @@
 export default function BudgetContainer() {
-  return <div>BudgetContainer</div>;
+  return (
+    <div>
+      <div className="bg-white rounded-md shadow-sm px-3 py-6 flex flex-col gap-3">
+        <span className="title1">월 소비 현황</span>
+        <span className="title3 text-primary">
+          예산을 등록하면 소비 현황을 알려드려요
+        </span>
+      </div>
+    </div>
+  );
 }
