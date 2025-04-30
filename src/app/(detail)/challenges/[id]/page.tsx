65f0605c1d737ca468d6b713ee00df7dd0241786
yeonna18k <yeonna18k@gmail.com>
2025-04-30 19:50:59 +0900

    feat: 챌린지 수정

--- src/app/(detail)/challenges/[id]/page.tsx
+++ src/app/(detail)/challenges/[id]/page.tsx
@@ -1,34 +1,42 @@
 'use client';
 
+import { DetailChallenge } from '@/api/ChallengeAPI';
 import { ActionButtons } from '@/components/goals/ActionButtons';
 import { ChallengeDetailHeader } from '@/components/goals/ChallengeDetailHeader';
 import { ChallengeProgress } from '@/components/goals/ChallengeProgress';
 import { ParticipantList } from '@/components/goals/ParticipantList';
 import { PaymentSection } from '@/components/goals/PaymentSection';
 import { PublicToggle } from '@/components/goals/PublicToggle';
-import { useDetailChallenge } from '@/hooks/useChallenges';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
 
 export default function ChallengeDetail() {
   const params = useParams();
   const id = Number(params?.id);
 
-  const { data: challenge, isLoading } = useDetailChallenge(id);
-  if (isLoading || !challenge) return <div>Loading…</div>;
+  // const { data: challenge, isLoading } = useDetailChallenge(id);
+  // if (isLoading || !challenge) return <div>Loading…</div>;
+
+  const { data: challenge, isLoading } = useQuery({
+    queryKey: ['challenge', id],
+    queryFn: () => DetailChallenge(id),
+  });
 
   return (
     <div className="pb-16 max-w-md mx-auto">
       <ChallengeDetailHeader title="챌린지 상세" />
-      <div className="p-4">
-        <ChallengeProgress challenge={challenge} />
-        <ParticipantList capacity={challenge.capacity} />
-        <PublicToggle
-          challengeId={String(challenge.id)}
-          isPublic={challenge.release === 'PUBLIC'}
-        />
-        <ActionButtons />
-        <PaymentSection totalSavings={27000} payments={[]} />
-      </div>
+      {challenge && (
+        <div className="p-4">
+          <ChallengeProgress challenge={challenge} />
+          <ParticipantList capacity={challenge.capacity} />
+          <PublicToggle
+            challengeId={String(challenge.id)}
+            isPublic={challenge.release === 'PUBLIC'}
+          />
+          <ActionButtons />
+          <PaymentSection totalSavings={27000} payments={[]} />
+        </div>
+      )}
     </div>
   );
 }
