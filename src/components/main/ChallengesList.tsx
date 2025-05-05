00388e310c8643c1d06ff20ca24f1e49b2646a67
yeonna18k <yeonna18k@gmail.com>
2025-05-05 18:08:04 +0900

    feat: 챌린지 관련 컴포넌트 및 레이아웃 추가, ESLint 규칙 수정

--- src/components/main/ChallengesList.tsx
+++ src/components/main/ChallengesList.tsx
@@ -26,12 +26,14 @@ export const ChallengeList: React.FC<ChallengeListProps> = ({ challenges }) => {
   }
 
   return (
-    <div className="px-3 py-6 flex flex-col gap-3 rounded-lg bg-white shadow-sm">
-      <Link href="/challenges" className="flex justify-between items-center">
+    <Link
+      href="/challenges"
+      className="px-3 py-6 flex flex-col gap-3 rounded-lg bg-white shadow-sm"
+    >
+      <div className="flex justify-between items-center">
         <h1 className="title1">참여중인 챌린지</h1>
         <ChevronsRight size={24} />
-      </Link>
-
+      </div>
       {(displayChallenges ?? []).length === 0 ? (
         <div className="rounded-md p-4 flex border items-center gap-2">
           <div className="rounded-full bg-primary/20 h-9 w-9 items-center flex justify-center">
@@ -59,6 +61,6 @@ export const ChallengeList: React.FC<ChallengeListProps> = ({ challenges }) => {
           />
         ))
       )}
-    </div>
+    </Link>
   );
 };
