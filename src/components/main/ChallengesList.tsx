f795ec185971d4138c3bbd72f70a94e42235d677
yeonna18k <yeonna18k@gmail.com>
2025-05-05 18:46:26 +0900

    feat: 컴포넌트 구조 개선 및 링크 수정, 스타일 업데이트

--- src/components/main/ChallengesList.tsx
+++ src/components/main/ChallengesList.tsx
@@ -26,14 +26,11 @@ export const ChallengeList: React.FC<ChallengeListProps> = ({ challenges }) => {
   }
 
   return (
-    <Link
-      href="/challenges"
-      className="px-3 py-6 flex flex-col gap-3 rounded-lg bg-white shadow-sm"
-    >
-      <div className="flex justify-between items-center">
+    <div className="px-3 py-6 flex flex-col gap-3 rounded-lg bg-white shadow-sm">
+      <Link href="/challenges" className="flex justify-between items-center">
         <h1 className="title1">참여중인 챌린지</h1>
         <ChevronsRight size={24} />
-      </div>
+      </Link>
 
       {(displayChallenges ?? []).length === 0 ? (
         <div className="rounded-md p-4 flex border items-center gap-2">
@@ -62,6 +59,6 @@ export const ChallengeList: React.FC<ChallengeListProps> = ({ challenges }) => {
           />
         ))
       )}
-    </Link>
+    </div>
   );
 };
