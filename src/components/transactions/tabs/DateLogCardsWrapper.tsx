166f5f852ab4892ad38396af8133ca1edff22394
yeonna18k <yeonna18k@gmail.com>
2025-05-13 23:21:12 +0900

    feat: 수입/지출 내역 가상화(virtualizer) 적용

--- src/components/transactions/tabs/DateLogCardsWrapper.tsx
+++ src/components/transactions/tabs/DateLogCardsWrapper.tsx
@@ -8,6 +8,7 @@ import animationLoadingData from '../../../../public/lottie/piggy_loading.json';
 import animationErrorData from '../../../../public/lottie/query_error.json';
 import TransactionsLogCard from './TransactionsLogCard';
 import { useInfiniteAccountBook } from '@/hooks/useInfiniteAccountBook';
+import { useVirtualizer, useWindowVirtualizer } from '@tanstack/react-virtual';
 
 interface DateProps {
   startDate: string;
@@ -17,6 +18,7 @@ interface DateProps {
 export default function DateLogCardsWrapper({ startDate, endDate }: DateProps) {
   const observerRef = useRef<IntersectionObserver | null>(null);
   const loadMoreRef = useRef<HTMLDivElement>(null);
+  const parentRef = useRef<HTMLDivElement>(null);
 
   const {
     allTransactions,
@@ -27,36 +29,60 @@ export default function DateLogCardsWrapper({ startDate, endDate }: DateProps) {
     isPending,
   } = useInfiniteAccountBook({ startDate, endDate });
 
-  const handleObserver = useCallback(
-    (entries: IntersectionObserverEntry[]) => {
-      const [target] = entries;
-      if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
-        fetchNextPage();
-      }
-    },
-    [fetchNextPage, hasNextPage, isFetchingNextPage]
-  );
+  const rowVirtualizer = useVirtualizer({
+    count: hasNextPage ? allTransactions.length : allTransactions.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 100,
+    overscan: 5,
+  });
 
   useEffect(() => {
-    const currentElement = loadMoreRef.current;
-
-    if (currentElement) {
-      observerRef.current = new IntersectionObserver(handleObserver, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1,
-      });
-
-      observerRef.current.observe(currentElement);
+    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+    if (
+      lastItem?.index >= allTransactions.length - 1 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
     }
+  }, [
+    hasNextPage,
+    fetchNextPage,
+    allTransactions.length,
+    isFetchingNextPage,
+    rowVirtualizer.getVirtualItems(),
+  ]);
 
-    return () => {
-      if (observerRef.current && currentElement) {
-        observerRef.current.unobserve(currentElement);
-        observerRef.current.disconnect();
-      }
-    };
-  }, [handleObserver]);
+  // const handleObserver = useCallback(
+  //   (entries: IntersectionObserverEntry[]) => {
+  //     const [target] = entries;
+  //     if (target.isIntersecting && hasNextPage && !isFetchingNextPage) {
+  //       fetchNextPage();
+  //     }
+  //   },
+  //   [fetchNextPage, hasNextPage, isFetchingNextPage]
+  // );
+
+  // useEffect(() => {
+  //   const currentElement = loadMoreRef.current;
+
+  //   if (currentElement) {
+  //     observerRef.current = new IntersectionObserver(handleObserver, {
+  //       root: null,
+  //       rootMargin: '0px',
+  //       threshold: 0.1,
+  //     });
+
+  //     observerRef.current.observe(currentElement);
+  //   }
+
+  //   return () => {
+  //     if (observerRef.current && currentElement) {
+  //       observerRef.current.unobserve(currentElement);
+  //       observerRef.current.disconnect();
+  //     }
+  //   };
+  // }, [handleObserver]);
 
   return (
     <>
@@ -73,10 +99,35 @@ export default function DateLogCardsWrapper({ startDate, endDate }: DateProps) {
         </div>
       )}
       {allTransactions.length > 0 && (
-        <div className="bg-white rounded-lg shadow-sm px-3 py-6 flex flex-col gap-4 flex-grow">
-          {allTransactions.map((transaction) => (
+        <div
+          ref={parentRef}
+          className="bg-white rounded-lg shadow-sm px-3 py-6 flex-grow"
+          style={{
+            height: `500px`,
+            width: `100%`,
+            overflow: 'auto',
+          }}
+        >
+          <div
+            className="flex flex-col gap-4"
+            style={{
+              height: `${rowVirtualizer.getTotalSize()}px`,
+              width: '100%',
+              position: 'relative',
+            }}
+          >
+            {/* {allTransactions.map((transaction) => (
             <TransactionsLogCard key={transaction.id} data={transaction} />
-          ))}
+          ))} */}
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              return (
+                <TransactionsLogCard
+                  key={virtualRow.index}
+                  data={allTransactions[virtualRow.index]}
+                />
+              );
+            })}
+          </div>
           <div ref={loadMoreRef} className="py-4 flex justify-center">
             {isFetchingNextPage && (
               <Lottie
